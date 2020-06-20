ad4b178b71caf0d2de3debefdc9e8d96d762e313
macrespo42 <maxime.crespopro@gmail.com>
2020-06-20 14:38:50 +0200

    ex02 write test for multiply and power

---
+++ lib/02_calculator.rb
@@ -0,0 +1,23 @@
+def add(nb1, nb2)
+	return nb1 + nb2
+end
+
+def subtract(nb1, nb2)
+	return nb1 - nb2
+end
+
+def sum(array_of_numbers)
+	total = 0
+	array_of_numbers.each do |number|
+		total += number
+	end
+	return total
+end
+
+def multiply(nb1, nb2)
+	return nb1 * nb2
+end
+
+def power(nb1, nb2)
+	return nb1 ** nb2
+end
--- spec/02_calculator_spec.rb
+++ spec/02_calculator_spec.rb
@@ -47,16 +47,22 @@ describe "#multiply" do
     expect(multiply(3, 4)).to eq(12)
   end
 
-  it "multiplies two other numbers"
+  it "multiplies two other numbers" do
     # one other test here, don't forget do end ;)
+    expect(multiply(7, 6)).to eq(42)
+  end
 
-  it "multiplies two other numbers, one of them being 0"
+  it "multiplies two other numbers, one of them being 0" do
     # one last test, with 0 in it
+    expect(multiply(2, 0)).to eq(0)
+  end
 
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+    expect(power(10, 2)).to eq(100)
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
